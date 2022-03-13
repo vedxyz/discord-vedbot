11870d56f236328d6e160f8de0c60ee3901c0f40
Ved <dev@vedat.xyz>
2022-03-13 08:02:51 +0300

    Add database layer

--- db/10-init.sql
+++ db/10-init.sql
@@ -6,24 +6,25 @@ CREATE TABLE servers (
 CREATE TABLE channels (
     channel_id varchar(20) PRIMARY KEY,
     server_id varchar(20) REFERENCES servers(server_id),
-    type varchar(30)
+    channel_type varchar(30)
 )
 
-CREATE TABLE mention_pics (
-    user_id varchar(20) PRIMARY KEY,
+CREATE TABLE mention_images (
+    image_id serial PRIMARY KEY,
+    user_id varchar(20),
     server_id varchar(20) REFERENCES servers(server_id)
-    url text,
+    image_url text,
 )
 
 CREATE TABLE rules (
     rule_id serial PRIMARY KEY,
     server_id varchar(20) REFERENCES servers(server_id)
-    rule_count smallint,
+    rule_index smallint,
     rule_content text,
 )
 
 CREATE TABLE roles (
     role_id varchar(20) PRIMARY KEY,
     server_id varchar(20) REFERENCES servers(server_id),
-    type varchar(30)
+    role_type varchar(30)
 )
